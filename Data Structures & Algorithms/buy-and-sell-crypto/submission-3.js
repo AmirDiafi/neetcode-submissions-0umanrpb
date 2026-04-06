59e9f5b48df5d3f4074b7b2b4e8542a595ad8319
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2026-04-06 12:02:41 +0100

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        const daysPricesObj = {}
+        for(let i = 0; i<prices.length;i++) {
+            const profitPrices = []
+            for(let j = i+1; j<prices.length;j++) {
+                if(prices[i]<prices[j]) {
+                    profitPrices.push(prices[j])
+                }
+            }
+            if(profitPrices.length>0) {
+                daysPricesObj[prices[i]] = Math.max(...profitPrices)
+            }
+            
+        }
+        console.log("daysPricesObj", daysPricesObj)
+
+        const maxProfitsDays = Object.entries(daysPricesObj)
+        console.log("maxProfitsDays", maxProfitsDays)
+        const profitValues = maxProfitsDays.map((v)=> {
+            return v[1] - v[0]
+        })
+        console.log("profitValues", profitValues)
+        if(maxProfitsDays.length === 0) return 0
+        console.log("max:", Math.max(...profitValues))
+        return Math.max(...profitValues)
+    }
+}
